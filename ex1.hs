fb27e628912a41241040181d619d730d3fad00c1
tomclima <tomecostalima1228@gmail.com>
2025-06-30 09:30:03 -0300

    a

---
+++ ex1.hs
@@ -0,0 +1,32 @@
+{- #########################################################
+
+        FirstScript.hs
+        Simon Thompson, August 2010.
+
+######################################################### -}
+
+module FirstScript where
+
+--      The value size is an integer (Integer), defined to be 
+--      the sum of twelve and thirteen.
+
+size :: Integer
+size = 12+13
+
+--      The function to square an integer.
+
+square :: Integer -> Integer
+square n = n*n
+
+--      The function to double an integer.
+        
+double :: Integer -> Integer
+double n = 2*n
+
+--      An example using double, square and size.
+
+func1 :: Integer -> Integer
+func1 n = square (double n)
+         
+example :: Integer
+example = double (size - square (2+2))
